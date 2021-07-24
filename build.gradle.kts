6f9a89d9eb4417f54f31f3b1991fac7db5f9e793
uinnn <uin.carrara@gmail.com>
2021-07-24 11:00:38 -0400

    1.2.1v

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,8 @@
-val excludeKotlin = Action<ExternalModuleDependency> {
-  exclude(group = "org.jetbrains.kotlin")
-}
-
-val excludeKotlinx = Action<ExternalModuleDependency> {
-  exclude(group = "org.jetbrains.kotlin")
-  exclude(group = "org.jetbrains.kotlinx")
-}
-
 plugins {
   kotlin("jvm") version "1.5.20"
   kotlin("plugin.serialization") version "1.5.20"
   id("java")
   id("com.github.johnrengelman.shadow") version "6.0.0"
-  id("me.bristermitten.pdm") version "0.0.33"
   id("maven-publish")
 }
 
@@ -27,11 +17,11 @@ repositories {
 dependencies {
   compileOnly("com.single.api:spigot:1.8.9")
   compileOnly(kotlin("stdlib-jdk8"))
-  pdm("org.jetbrains.kotlin:kotlin-reflect:1.5.20")
-  pdm("org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.2", excludeKotlin)
-  pdm("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2", excludeKotlin)
-  pdm("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.2.2", excludeKotlin)
-  pdm("com.charleskorn.kaml:kaml:0.34.0")
+  compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.5.20")
+  compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.2")
+  compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+  compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.2.2")
+  compileOnly("com.charleskorn.kaml:kaml:0.34.0")
 }
 
 tasks {
@@ -47,16 +37,10 @@ tasks {
   }
 
   compileKotlin {
-    kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.time.ExperimentalTime," +
-      "kotlin.ExperimentalStdlibApi," +
-      "kotlinx.coroutines.DelicateCoroutinesApi," +
-      "kotlinx.coroutines.ExperimentalCoroutinesApi," +
+    kotlinOptions.freeCompilerArgs +=
+      "-Xopt-in=kotlin.ExperimentalStdlibApi," +
       "kotlinx.serialization.ExperimentalSerializationApi," +
       "kotlinx.serialization.InternalSerializationApi"
-  }
-
-  withType<GenerateModuleMetadata> {
-    enabled = false
   }
 
   shadowJar {
@@ -64,6 +48,5 @@ tasks {
     archiveName = "${project.name}.jar"
     baseName = project.name
     version = project.version.toString()
-    dependsOn(pdm)
   }
 }
